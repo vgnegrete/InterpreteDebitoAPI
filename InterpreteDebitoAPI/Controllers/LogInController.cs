718a3716799a44a2876ac55662e8983b1ab3184b
gn <vgnegrete@hotmail.com>
2024-06-10 13:48:41 -0600

    LogIn

---
+++ InterpreteDebitoAPI/Controllers/LogInController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using InterpreteDebitoAPI.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace InterpreteDebitoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogInController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public LogInController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        public ActionResult<LogInResponseDTO> LogIn(LoginRequestDTO pLogIn)
+		{
+            try
+            {
+                if (ValidaLoginApp(pLogIn))
+                {
+                    var tok = GenerarTokenJWT();
+
+                    return Ok(new LogInResponseDTO() { Result = 0, Mensaje = "Ok", tokenInfo = tok });
+                }
+                else
+                {
+                    return Unauthorized(new LogInResponseDTO() { Result = 1, Mensaje = "Usuario no autorizado", tokenInfo = null });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new LogInResponseDTO() { Result =1, Mensaje = ex.Message, tokenInfo = null});
+            }
+        }
+
+        private bool ValidaLoginApp(LoginRequestDTO pLogIn)
+        {
+            if (pLogIn?.User?.ToLower() == "usuario" && pLogIn.Password == "Abcde1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // GENERAMOS EL TOKEN CON LA INFORMACIÓN DEL USUARIO
+        private TokenInfo GenerarTokenJWT()
+        {
+            // CREAMOS EL HEADER //
+            var _symmetricSecurityKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(configuration["SecretKet"])
+                );
+            var _signingCredentials = new SigningCredentials(
+                    _symmetricSecurityKey, SecurityAlgorithms.HmacSha256
+                );
+            var _Header = new JwtHeader(_signingCredentials);
+
+            // CREAMOS LOS CLAIMS //
+            var _Claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            // Exipra a la 24 horas.
+            DateTime Caducidad = DateTime.UtcNow.AddHours(24);
+
+            // CREAMOS EL PAYLOAD //
+            var _Payload = new JwtPayload(
+                    issuer: "",
+                    audience: "",
+                    claims: _Claims,
+                    notBefore: DateTime.UtcNow,
+                    expires: Caducidad
+                );
+
+            // GENERAMOS EL TOKEN //
+            var _Token = new JwtSecurityToken(
+                    _Header,
+                    _Payload
+                );
+
+            return new TokenInfo()
+            {
+
+                Token = new JwtSecurityTokenHandler().WriteToken(_Token),
+                fechaCaducidad = Caducidad
+
+            };
+        }
+    }
+}
+
---
+++ InterpreteDebitoAPI/Entities/DTOS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace InterpreteDebitoAPI.Entities
+{
+	public class GenericResponse
+	{
+		public int Result { get; set; }
+        public string? Mensaje { get; set; }
+    }
+
+	public class TokenInfo
+	{
+		public DateTime fechaCaducidad { get; set; }
+		public string? Token { get; set; }
+	}
+
+	public class LoginRequestDTO
+	{
+		public string? User { get; set; }
+        public string? Password { get; set; }
+
+    }
+
+    public class LogInResponseDTO : GenericResponse
+	{
+		public TokenInfo? tokenInfo { get; set; }
+	}
+
+	public class TiemposEjecucion
+	{
+		public double Min { get; set; }
+        public double Max { get; set; }
+        public double Promedio { get; set; }
+		public DateTime UltimaTransaccion { get; set; }
+    }
+
+	public class CantidadxTipo
+	{
+		public string? CodigoOperacion { get; set; }
+		public int Cantidad { get; set; }
+	}
+
+	public class DashboardResponseDTO : GenericResponse
+	{
+		public TiemposEjecucion? tiemposEjecucion { get; set; }
+		public List<CantidadxTipo>? LstOperacionesTipo {get; set;}
+	}
+
+}
+
