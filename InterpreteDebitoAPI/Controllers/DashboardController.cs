8a23c464cf257614f025f8b660d8ccda8c089479
gn <vgnegrete@hotmail.com>
2024-06-10 14:23:26 -0600

    Dummy dashboard

---
+++ InterpreteDebitoAPI/Controllers/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using InterpreteDebitoAPI.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterpreteDebitoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardController : ControllerBase
+    {
+        [HttpGet, Authorize]
+        public ActionResult<DashboardResponseDTO> GetDashboard()
+		{
+            Random rnd = new Random();
+            double MinRandom = rnd.Next(1, 30)/10.0;
+            double MaxRandom = MinRandom + rnd.Next(1, 20) / 10.0;
+
+            return Ok(new DashboardResponseDTO()
+            {
+                Result = 0,
+                Mensaje = "Ok",
+                tiemposEjecucion = new TiemposEjecucion() { Min =MinRandom, Max = MaxRandom, Promedio = (MinRandom + MaxRandom)/2.0, UltimaTransaccion = DateTime.Now },
+                LstOperacionesTipo = new List<CantidadxTipo>() {
+                    new CantidadxTipo() { CodigoOperacion="00", Cantidad = rnd.Next(60, 100) },
+                    new CantidadxTipo() { CodigoOperacion="01", Cantidad = rnd.Next(40, 80) },
+                    new CantidadxTipo() { CodigoOperacion="17", Cantidad = rnd.Next(40, 120) },
+                    new CantidadxTipo() { CodigoOperacion="45", Cantidad = rnd.Next(5, 10) },
+                    new CantidadxTipo() { CodigoOperacion="65", Cantidad = rnd.Next(10, 20) },
+                    new CantidadxTipo() { CodigoOperacion="20", Cantidad = rnd.Next(1, 5) },
+                    new CantidadxTipo() { CodigoOperacion="25", Cantidad = rnd.Next(50, 70) }
+                }
+
+            });
+
+		}
+	}
+}
+
--- InterpreteDebitoAPI/Program.cs
+++ InterpreteDebitoAPI/Program.cs
@@ -1,11 +1,55 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKet"])),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +62,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
